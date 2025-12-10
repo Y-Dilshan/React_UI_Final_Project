2f08fb08ab727feee7ff0f0e5e45b7956496b454
Yasindu Dilshan <dilshanyasindu23@gmail.com>
2025-12-10 19:25:55 +0530

    completed project

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './Home.css';  // Import Home.css, NOT Header.css!
+import './Home.css';
 
 function Home() {
   return (
